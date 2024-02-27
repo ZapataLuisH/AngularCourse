352fe58453d77ffe60624f199ebbe041a7d35668
LuisHZapata <zapataluish@gmail.com>
2024-02-27 11:21:39 -0500

    First version

--- todoApp/src/app/app.component.ts
+++ todoApp/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todoApp';
+  welcome = 'Hola dice LuisH desde Programacion Angular';
+  tasks = [
+    'Instalar el angular CLI',
+    'Crear el proyecto',
+    'Finalizar'
+  ]
 }
